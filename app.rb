5d0a7a8c4defb7364d18486570b52b95a3cc2c41
Katie Hoffman <ktahoffman@gmail.com>
2014-11-25 11:08:03 -0500

    add 12 programmers

--- app.rb
+++ app.rb
@@ -3,10 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get "/" do
-    binding.pry
     @programmers = Programmers.run
-
-    @i = 0
     erb :'index'
   end
 
--- lib/programmers.rb
+++ lib/programmers.rb
@@ -1,13 +1,19 @@
 class Programmers
 
   def self.run
-    coders = YAML.load_file('./lib/programmers.yml').each_with_object([]) do |k,a| 
-      a << {:pic = slugify(k), :name => k}
+    coders = YAML.load_file('./lib/programmers.yml').each_with_object([]) do |name,a| 
+      slugged_name = slugify(name)
+      a << {slugged_name => {name: name}}
+      a << {slugged_name => {img: get_image_file(name)}}
     end
-    coders
+    coders.shuffle
   end
 
-  def slugify(name)
+  def self.get_image_file(name)
+    "/images/#{slugify(name)}.jpg" 
+  end
+
+  def self.slugify(name)
     name.downcase.gsub(" ", "-")
   end
   
