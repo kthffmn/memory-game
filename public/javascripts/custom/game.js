93b1b2b4de12630e39ce8cf42a43b316f63d77e3
Katie Hoffman <ktahoffman@gmail.com>
2014-11-25 17:54:52 -0500

    add flip-back-over functionality

--- public/javascripts/custom/game.js
+++ public/javascripts/custom/game.js
@@ -1,8 +1,18 @@
 $( document ).ready(function() {
 
   $('.flip').click(function(){
-    var card = $(this).find('.card');
+    var card = $(this).find('.card')
     card.addClass('flipped');
   });
-  
+
+  function turnDownCards() {
+    if($('.flipped').length > 1) {
+      $(".card").removeClass("flipped");
+    }
+  }
+
+  setInterval(function() { 
+    turnDownCards() 
+  }, 2000); 
+
 });
