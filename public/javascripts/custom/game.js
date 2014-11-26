7c6c4734279af516b7d2869ff68c311886cfaee2
Katie Hoffman <ktahoffman@gmail.com>
2014-11-26 12:02:13 -0500

    working on js

--- public/javascripts/custom/game.js
+++ public/javascripts/custom/game.js
@@ -1,18 +1,23 @@
 $( document ).ready(function() {
 
+  var visibleCardNum = 0;
+
   $('.flip').click(function(){
-    var card = $(this).find('.card')
-    card.addClass('flipped');
+    var card = $(this).find('.card');
+
+    if(!card.hasClass("flipped") && visibleCardNum < 2 && !card.hasClass("matched")) {
+      card.addClass('flipped');
+      visibleCardNum += 1;
+
+      if(visibleCardNum == 2) {
+        setTimeout(function() {
+          $('.card').removeClass('flipped');
+          visibleCardNum = 0;
+        }, 1500);
+      }
+
+    }
+
   });
-
-  // function turnDownCards() {
-  //   if($('.flipped').length > 1) {
-  //     $(".card").removeClass("flipped");
-  //   }
-  // }
-
-  // setInterval(function() { 
-  //   turnDownCards() 
-  // }, 2000); 
 
 });
