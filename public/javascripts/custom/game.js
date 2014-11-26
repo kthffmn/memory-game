a3311229dcada79e898db5eead5efd822ef50278
Katie Hoffman <ktahoffman@gmail.com>
2014-11-26 13:15:13 -0500

    functioning game, no winner settings

--- public/javascripts/custom/game.js
+++ public/javascripts/custom/game.js
@@ -1,19 +1,34 @@
 $( document ).ready(function() {
 
   var visibleCardNum = 0;
+  var flippedCard;
 
   $('.flip').click(function(){
     var card = $(this).find('.card');
 
     if(!card.hasClass("flipped") && visibleCardNum < 2 && !card.hasClass("matched")) {
+
       card.addClass('flipped');
       visibleCardNum += 1;
 
-      if(visibleCardNum == 2) {
-        setTimeout(function() {
-          $('.card').removeClass('flipped');
-          visibleCardNum = 0;
-        }, 1500);
+      if(visibleCardNum == 1) { 
+        flippedCard = card;
+      } else if(visibleCardNum == 2) {
+        if(card.data("programmer") == flippedCard.data("programmer")) {
+          setTimeout(function() {
+            $.each([card, flippedCard], function(i, c){
+              c.addClass("matched");
+            });
+            visibleCardNum = 0;
+          }, 325);
+        } else {
+          setTimeout(function() {
+            $.each([card, flippedCard], function(i, c){
+              c.removeClass("flipped")
+            });
+            visibleCardNum = 0;
+          }, 1500);
+        }
       }
 
     }
